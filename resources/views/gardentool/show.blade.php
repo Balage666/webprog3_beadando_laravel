8d0d96e32f47478d36a031fc835cb23f881e8207
Balázs Karácsony (H8HYMB) <karacsonybalazs01@gmail.com>
2023-05-12 18:30:59 +0200

    Updated blade files
    Minor fixes

--- resources/views/gardentool/show.blade.php
+++ resources/views/gardentool/show.blade.php
@@ -38,7 +38,7 @@
             </div>
           </div>
             <div class="card-footer d-flex justify-content-center gap-2">
-                <button onclick="history.back();" class="btn btn-secondary btn-lg">Back to Storefront <i class="fa-solid fa-arrow-left"></i> </button>
+                <button onclick="history.back();" class="btn btn-secondary btn-lg">Go Back <i class="fa-solid fa-arrow-left"></i> </button>
                 <a href="/cart/add/{{ $gardenTool->id }}" class="btn {{ $gardenTool->stock == 0 ? "btn-danger" : "btn-primary" }}  btn-block btn-lg">Add to Cart <i class="fa-solid fa-cart-plus"></i> </a>
             </div>
         </div>
