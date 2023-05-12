8571b1dc70baf0469a42344c9615900cc7ce3094
Balázs Karácsony (H8HYMB) <karacsonybalazs01@gmail.com>
2023-05-12 18:32:34 +0200

    Created order migration file

---
+++ database/migrations/2023_05_10_122607_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->longText('cart');
+            $table->string('user_identifier');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('address');
+            $table->string('phone_number');
+            $table->longText('additional_information');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
