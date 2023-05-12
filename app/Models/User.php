a7ac59dac104632d2fcf3e52aea6cf5d384e9e4d
Balázs Karácsony (H8HYMB) <karacsonybalazs01@gmail.com>
2023-05-12 18:34:59 +0200

    Modifed User model, Added Orders relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,4 +43,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function Orders() {
+        $this->hasMany(Order::class, 'user_identifier', 'email');
+    }
 }
