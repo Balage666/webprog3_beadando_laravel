592e789121848c9a3ca78e5527c7a6e8cc33f0d6
Balázs Karácsony (H8HYMB) <karacsonybalazs01@gmail.com>
2023-05-12 18:32:06 +0200

    Created middleware to check the status of cart

---
+++ app/Http/Middleware/CheckCartItems.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckCartItems
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!request()->session()->has('cart')) {
+            return redirect('/')->with('access-checkout-failed', 'You have no items in your cart!');
+        }
+
+        return $next($request);
+    }
+}
